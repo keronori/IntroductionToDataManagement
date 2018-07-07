a0f72aa947fe74ba6b0eb05395e1d75b69741e3b
noritaka <33963908+keronori@users.noreply.github.com>
2018-07-07 14:44:51 +0900

    Update DiceGame.java
    Won/lost message modified

--- DiceGame.java
+++ DiceGame.java
@@ -3,6 +3,7 @@ import java.io.*;
 class DiceGame{
   public static void main(String[] args){
     int dice1,dice2;
+    String str1="No name";
     
     BufferedReader d = new BufferedReader(new InputStreamReader(System.in));
     System.out.println("What is your name?");
@@ -10,8 +11,10 @@ class DiceGame{
     try{
       String str=d.readLine();
       System.out.println("Hello, "+str+"!");
+      str1=str;
     }catch(IOException ioe){
       System.out.println("Error");
+      System.exit(1);
     }
 
     System.out.println("Rolling the dice...");
@@ -20,5 +23,12 @@ class DiceGame{
     System.out.println("Die 1: "+dice1);
     System.out.println("Die 2: "+dice2);
     System.out.println("Total value: "+(dice1+dice2));
+    
+    if((dice1+dice2)>7){
+      System.out.println(str1+" won!");
+    }else{
+      System.out.println(str1+ " lost!");
+    }
+
   }
 }
