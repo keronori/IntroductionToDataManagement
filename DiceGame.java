221d39e70eaf798996e4eea557999f0c02f8e6d3
noritaka <33963908+keronori@users.noreply.github.com>
2018-07-07 14:27:06 +0900

    Update DiceGame.java
    Greetings added

--- DiceGame.java
+++ DiceGame.java
@@ -1,7 +1,19 @@
 import java.lang.Math;
+import java.io.*;
 class DiceGame{
   public static void main(String[] args){
     int dice1,dice2;
+    
+    BufferedReader d = new BufferedReader(new InputStreamReader(System.in));
+    System.out.println("What is your name?");
+    System.out.print("> ");
+    try{
+      String str=d.readLine();
+      System.out.println("Hello, "+str+"!");
+    }catch(IOException ioe){
+      System.out.println("Error");
+    }
+
     System.out.println("Rolling the dice...");
     dice1=(int)(1+Math.random()*6);
     dice2=(int)(1+Math.random()*6);
